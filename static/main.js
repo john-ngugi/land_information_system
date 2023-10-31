5f537a0b79dc06a5f931d8040f096a6e5e30fa18
john-ngugi <johnngugi0407@gmail.com>
2023-10-31 15:55:40 +0300

    initial commit

---
+++ static/main.js
@@ -0,0 +1,4 @@
+var map = L.map('map').setView([-4.09, 39.505], 16);
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(map);
